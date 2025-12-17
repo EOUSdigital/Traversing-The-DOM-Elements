8dc8b5d52baaf42b40613d5d554e77a5e2d70140
EOUSdigital <117541034+EOUSdigital@users.noreply.github.com>
2025-12-17 21:04:17 +0000

    Add files via upload

---
+++ app.js
@@ -0,0 +1,49 @@
+"use strict";
+
+//TODO  Step 5. Guided Practice – Using Your Current HTML
+
+//  Assume your current HTML (the one you just showed: nav list, tasks, grid with cards, buttons) and a single app.js.
+//  Please implement the following steps in app.js. You can keep your previous Lesson 05 code and add these beneath it, or comment some parts out if the console feels noisy.
+
+//? Task 1 – Parent and Children
+
+//  1. Select the section with the cards:
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
